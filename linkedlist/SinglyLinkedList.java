20cf8f44f8bad4e97bf035ad25c3c43975eb2bb8
Jacob Schneck <jschneck@Gerald.localdomain>
2021-06-02 15:34:20 -0500

    SinglyLinkedList Finished, needs testing

--- linkedlist/SinglyLinkedList.java
+++ linkedlist/SinglyLinkedList.java
@@ -2,15 +2,18 @@ package linkedlist;
 
 import node.Node; 
 
-public class SinglyLinkedList<T> {
+public class SinglyLinkedList<T extends Comparable<T>> {
+    /************** Instance Variables **************/
     private Node<T> head;
 
+    /************** Constructors ********************/
     public SinglyLinkedList(Node<T> newHead) {
         this.head = newHead;
     }
 
+    /************** LIFO Methods *******************/
     public void push(Node<T> node) {
-        Node<T> curr = head;
+        Node<T> curr = head; // soft coppies head
         node.next = curr;
         head = node;
     }
@@ -30,6 +33,63 @@ public class SinglyLinkedList<T> {
         return temp;
     }
 
+    /************** Insert, Remove and Append ******/
+    public boolean insert(Node<T> node, int pos) {
+        if (pos == 0) {
+            push(node);
+            return true;
+        }
+        boolean inserted = false; 
+        Node<T> curr = head;
+        int count = 0;
+        while (curr != null && inserted == false) {
+            if (count == pos - 1) { // looking for node that links to the position (not position itself)
+                var temp = curr.next;
+                node.next = temp;
+                curr.next = node;
+                inserted = true;
+            }
+            curr = curr.next;
+            count += 1;            
+        }
+
+        return inserted;
+    }
+
+    public boolean remove(T val) {
+        boolean removed = false;
+        Node<T> curr = head;        
+        Node<T> prev = null;
+
+        if (curr.val.compareTo(val) == 0) { // first node
+            head = head.next;
+            return true;
+        }
+
+        while (curr != null && removed == false) {
+            if (curr.val.compareTo(val) == 0) {
+                prev.next = curr.next;
+                curr = prev.next;
+                removed = true;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return removed;
+    }
+
+    public void append(Node<T> node) {
+        Node<T> curr = head;
+        while (curr.next != null){
+            curr = curr.next;
+        }
+        curr.next = node;
+    }
+
+    /************** To String Method ***************/
+    @Override
     public String toString() {
         String result = "";
         var curr = this.head;
@@ -39,13 +99,13 @@ public class SinglyLinkedList<T> {
             } else {
                 result += curr.val.toString() + " -> ";
             }
-
             curr = curr.next;
         }
 
         return result;
     }
 
+    /************** Main Method ********************/
     public static void main(String[] args) {
         Node<Integer> n0 = new Node<Integer>(3);
         Node<Integer> n1 = new Node<Integer>(4);
@@ -55,9 +115,40 @@ public class SinglyLinkedList<T> {
         SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>(n0);
         // list.head = n0;
         Node<Integer> newNode = new Node<Integer>(2);
+
         list.push(newNode);
-        System.out.println(list.toString());
+        System.out.println(list);
+
         list.pop();
         System.out.println(list);
+
+        System.out.println(list.insert(new Node<Integer>(5), 2));
+        System.out.println(list);
+        System.out.println(list.insert(new Node<Integer>(5), 5));
+        System.out.println(list);
+        System.out.println(list.insert(new Node<Integer>(9), 0));
+        System.out.println(list);
+
+        System.out.println(list.remove(2));
+        System.out.println(list);
+        System.out.println(list.remove(4));
+        System.out.println(list);
+        System.out.println(list.remove(7));
+        System.out.println(list);
+        System.out.println(list.remove(9));
+        System.out.println(list);
+
+        list.append(new Node<Integer>(6));
+        System.out.println(list);
+        list.append(new Node<Integer>(7));
+        System.out.println(list);
+
+        SinglyLinkedList<String> stringList = new SinglyLinkedList<String>(new Node<String>("Hello"));
+        stringList.append(new Node<String>("World"));
+        System.out.println(stringList);
+        stringList.remove("Hello");
+        stringList.push(new Node<String>("Goodbye"));
+        stringList.insert(new Node<String>("Cruel"), 1); 
+        System.out.println(stringList);
     }
 }
